271373ac28b14f4817354c7ea3ba28a252115828
rtanubra <rdtanubrata@gmail.com>
2019-06-19 21:05:29 -0400

    structure and context

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+
+import './App.css'
+
+class App extends Component{
+  render(){
+      return (
+          <>
+            <h1>Hello World</h1>
+          </>
+          )
+  }
+}
+
+export default App
---
+++ src/contexts/trafus_context.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+const TrafusContext = React.createContext({
+    trafus_teams : [{
+        "name":'test_1_team',
+        "id":1
+    }],
+    trafus_users :[
+        {"full_name":"Test One The Human",
+        "user_name":"test_1",
+        "id":1,
+        "team_id":1,
+        "password":"password"}
+    ],
+    trafus_categories :[
+        {
+            "name":"Food and Wine",
+            "id":1,
+            "team_id":1,
+            "budget":1500,
+        },
+        {
+            "name":"transportation",
+            "id":2,
+            "team_id":1,
+            "budget":500
+        },
+        {
+            "name":"groceries",
+            "id":3,
+            "team_id":1,
+            "budget":500
+        }
+
+    ],
+    trafus_expenses:[]
+})
+
+export default TrafusContext
