46f5d45073cf309e847778de6c785136966495d3
rtanubra <rdtanubrata@gmail.com>
2019-08-06 08:05:14 -0400

    fixed services issue given bad input

--- src/services/date-services.js
+++ src/services/date-services.js
@@ -1,6 +1,9 @@
 const DateService = {
     createDate(dateString){
         const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+        if(!dateString){
+            return dateString
+        }
         const year = dateString.slice(2,4)
         const month = dateString.slice(5,7)
         const date = dateString.slice(8,10)
