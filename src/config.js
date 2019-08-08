80ab1a8977f63adc76d6d032a41a7491e33071fe
rtanubra <rdtanubrata@gmail.com>
2019-08-07 20:03:35 -0400

    cleaned config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
 export default {
   API_ENDPOINT: process.env.NODE_ENV==="development"?process.env.REACT_APP_BASE_URL_DEV:process.env.REACT_APP_BASE_URL,
-  //use this to explicitly work production
-  //API_ENDPOINT: process.env.REACT_APP_BASE_URL,
-  //use this to explicitly work development
-  //API_ENDPOINT:process.env.REACT_APP_BASE_URL_DEV,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
